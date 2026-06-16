0989fb0288c221bc0698d6bc85347face88ae829
Dm2787 <damianmuttke@gmail.com>
2026-06-16 15:50:06 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'czas-pracy-v8'; // Zmieniłem wersję na v3, żeby zmusić przeglądarkę do odświeżenia cache
+const CACHE_NAME = 'czas-pracy-v9'; // Zmieniłem wersję na v3, żeby zmusić przeglądarkę do odświeżenia cache
 const FILES_TO_CACHE = [
   './',
   './index.html',
